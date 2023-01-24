9ebd2c2c25833416fa579557aeacc62efa6b4dce
Evan McKenzie <evan.mcken@gmail.com>
2023-01-24 11:32:08 -0800

    completed instars

--- crush depth.R
+++ crush depth.R
@@ -12,23 +12,55 @@ ggplot(data = crush_raw,
        aes(x = larva, 
            y = head_cap_len, 
            colour = species)) +
-  geom_point()
-  theme_classic()
+  geom_point() #+
+  #theme_classic()
 
 
 ##################    asigning instar based on head_cap_len and species    #################
 
+crush_raw <- crush_raw %>% 
+  mutate(instar = replace(instar, 
+                          head_cap_len > 1.5 & 
+                            head_cap_len < 2 & 
+                            species == "trivittatus", # americanus and trivittatus based on Fedorenko and Swift 1972
+                          4)) %>%
+  mutate(instar = replace(instar, 
+                           head_cap_len > 0.75 & 
+                             head_cap_len < 1 & 
+                             species == "trivittatus", 
+                          3)) %>%
+  mutate(instar = replace(instar, 
+                          head_cap_len > 0.5 & 
+                            head_cap_len < 0.75 & 
+                            species == "trivittatus", 
+                          2)) %>%
+  mutate(instar = replace(instar, 
+                          head_cap_len > 1.35 & 
+                            head_cap_len < 2 & 
+                            species == "americanus",
+                          4)) %>%
+  mutate(instar = replace(instar, 
+                          head_cap_len > 1 & 
+                            head_cap_len < 1.35 & 
+                            species == "americanus", 
+                          3)) %>% 
+  mutate(instar = replace(instar,
+                          head_cap_len > 0.5 &
+                            head_cap_len < 1 &
+                            species == "americanus",
+                          2)) %>%
+  mutate(instar = replace(instar,
+                          head_cap_len > 0.5 &
+                            head_cap_len < 1 &
+                            species == "punctipenis",
+                          3)) %>%
+  mutate(instar = replace(instar,
+                          head_cap_len > 0 &
+                            head_cap_len < 0.5 &
+                            species == "punctipenis",
+                          2))
 
-  crush_raw <- crush_raw %>% 
-    mutate(instar = replace(instar, 
-                            head_cap_len > 1.5 & 
-                              head_cap_len < 2 & 
-                              species == "trivittatus", 
-                            4)) %>%
-    mutate(instar = replace(instar, 
-                            head_cap_len > 1.35 & 
-                              head_cap_len < 2 & 
-                              species == "americanus", 
-                            4))
-    
+
+
+
   
