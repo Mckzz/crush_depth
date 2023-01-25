d31df77f20322ffb11c0f44836b7995ec8cbb12c
Evan McKenzie <evan.mcken@gmail.com>
2023-01-24 20:15:30 -0800

    long form dataset with dimensions and crush atm

--- crush depth.R
+++ crush depth.R
@@ -60,7 +60,90 @@ crush_raw <- crush_raw %>%
                             species == "punctipenis",
                           2))
 
+print(crush_raw)
 
+###########     simple scatter of dimensions with crush depth    ###############
 
+# make separate data frames for crush-pressure and dimensions 
+# so they can be pivoted separately and then recombined
 
+crush_raw.atm <- crush_raw %>% ###    Maybe still need to make ant.post a vector...
+  select(1:9) %>% # take only crush atm values
+  pivot_longer(cols=c(`antR`, `antL`, `postR`, `postL`), 
+               names_to = "sac", values_to = "atm") %>%
+  mutate(ant.post = "NA") %>% 
+  mutate(ant.post = replace(ant.post,
+                            startsWith(sac, "a"),
+                            'ant')) %>%
+  mutate(ant.post = replace(ant.post,
+                            startsWith(sac, "p"),
+                            'post')) %>%
+  mutate(side = "NA") %>%
+  mutate(side = replace(side,
+                            endsWith(sac, "R"),
+                            'right')) %>%
+  mutate(side = replace(side,
+                            endsWith(sac, "L"),
+                            'left')) %>%
+  slice(rep(1:n(), each = 2)) %>% # so that an ant or post sac on a certain side can have both an area and diameter
+  select(larva, site, species, head_cap_len, instar, ant.post, side, atm) %>%
+  arrange(larva, side, by_group = T)
   
+crush_raw.atm$sac <- NULL
+  
+print(crush_raw.atm, n = 20)
+
+
+crush_raw.dimen <- crush_raw %>% 
+  select(1:5, 10:13) %>% # take only dimension values
+  pivot_longer(cols=c(`ant_area`, `ant_diameter`, `post_area`, `post_diameter`), 
+               names_to = "dimension", values_to = "value") %>%
+  mutate(ant.post = "NA") %>% 
+  mutate(ant.post = replace(ant.post,
+                            startsWith(dimension, "a"),
+                            'ant')) %>%
+  mutate(ant.post = replace(ant.post,
+                            startsWith(dimension, "p"),
+                            'post')) %>%
+  mutate(dimension = replace(dimension,
+                        endsWith(dimension, "a"),
+                        'area')) %>%
+  mutate(dimension = replace(dimension,
+                        endsWith(dimension, "r"),
+                        'diameter')) %>%
+  slice(rep(1:n(), times = 2)) %>% # times instead of each, to so that area and diameter alternate as they should to combine with atm df
+  select(larva, site, species, head_cap_len, instar, ant.post, dimension, value) %>%
+  arrange(larva)
+
+print(crush_raw.dimen, n = 20)
+
+# recombine for single data frame
+crush_raw.long <- crush_raw.atm %>% 
+  mutate(dimension = crush_raw.dimen$dimension) %>% # assumed to be the same for left and right airsacs
+  mutate(value = crush_raw.dimen$value)
+  
+
+print(crush_raw.long)
+
+# for plotting using area
+area.df <- crush_raw.long %>%
+  filter(dimension == "area")
+print(area.df)
+
+# for plotting using diameter
+diameter.df <- crush_raw.long %>%
+  filter(dimension == "diameter")
+print(diameter.df)
+
+ggplot(data = diameter.df,
+       aes(x = value, 
+           y = atm, 
+           colour = species)) +
+  geom_point()
+
+
+
+
+
+
+
